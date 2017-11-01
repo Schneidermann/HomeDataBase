d1dc64823c3b2d910fd32bb30cf6b9489ac7da6c
mauro m suzuki <msndomauro@hotmail.com>
2017-11-01 16:02:31 -0200

    Models

---
+++ BackEndHome/BackEndHome/Models/Dados.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BackEndHome.Models
+{
+	public class Dados
+	{
+		 public int DadosID { get; set; }
+ 
+         public string Nome { get; set; }
+ 
+         public string Descricao { get; set; }
+
+        public int Poligonos { get; set; }
+ 
+		public int Tamanho { get; set; }
+	}
+}
---
+++ BackEndHome/BackEndHome/Models/TipoDados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEndHome.Models
+{
+	public class TipoDados
+	{
+		public int TipoDadosID { get; set; }
+ 
+         public string Nome { get; set; }
+	}
+}
