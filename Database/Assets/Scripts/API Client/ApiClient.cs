efd8052a6888ef2cb64e5f9ffafcee59bb3555ec
Gabriel <aschneidergabriel@gmail.com>
2017-11-01 22:04:59 -0200

    Mostrando a descrição do personagem na tela

---
+++ Database/Assets/Scripts/API Client/ApiClient.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApiClient : MonoBehaviour {
+
+
+	const string baseurl = "http://localhost:52672//API";
+    public Text Nome;
+    public Text Descricao;
+    public Text id;
+    public Text poligonos;
+    public Text tamanho;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GetItems ());
+	}
+
+	IEnumerator GetItems()
+	{
+		UnityWebRequest request = UnityWebRequest.Get (baseurl + "/Dados");
+		yield return request.Send();
+
+		if (request.isNetworkError || request.isHttpError) {
+			Debug.Log (request.error);
+		} else 
+		{
+			string response = request.downloadHandler.text;
+			Debug.Log (response);
+			byte[] bytes = request.downloadHandler.data;
+
+			Item[] itens = JsonHelper.getJsonArray<Item> (response);
+			foreach (Item i in itens)
+			{
+				ImprimirItem (i);
+			}
+		}
+
+
+	}
+
+
+	private void ImprimirItem(Item i)
+	{
+		Debug.Log ("====== Dados Objeto ======");
+		Debug.Log ("Tipo: " + i.TipoItemID);
+		Debug.Log ("Nome: " + i.Nome);
+		Debug.Log ("Descrição: " + i.Descricao);
+		Debug.Log ("Poligonos: " + i.Poligonos);
+		Debug.Log ("Raridade: " + i.Tamanho);
+
+        Nome.text = "Nome: " + i.Nome;
+        Descricao.text = "Descrição: " + i.Descricao;
+        poligonos.text = "Poligonos: " + i.Poligonos.ToString();
+        tamanho.text = "Tamanho: " + i.Tamanho.ToString();
+        id.text = "ItemID: " + i.TipoItemID.ToString();
+    }
+
+}
---
+++ Database/Assets/Scripts/API Client/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿[System.Serializable]
+public class Item{
+
+
+	public int ItemID; 
+
+	public string Nome;
+
+	public string Descricao;
+
+	public int Poligonos; 
+
+	public int Tamanho; 
+
+	// Relacionamento Item --> TipoItem
+	public int TipoItemID; 
+
+
+
+}
+
+
